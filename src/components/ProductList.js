4cd70b211fe2ced16d96ebbebaa5c24d2a6498bf
H1rai <bakushota110ia@gmail.com>
2023-07-08 02:43:33 +0900

    要素の例後調整

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,50 +1,35 @@
-import React, { useState } from 'react';
-import '../styles/ProductList.css'; // スタイルの読み込み
+import React from 'react';
+import '../styles/ProductList.css';
+
+const calculateColumnCount = () => {
+  const screenWidth = window.innerWidth;
+  if (screenWidth >= 1200) {
+    return 5;
+  } else if (screenWidth >= 992) {
+    return 4;
+  } else if (screenWidth >= 768) {
+    return 3;
+  } else {
+    return 2;
+  }
+};
 
 const ProductList = ({ products }) => {
-  const [selectedProduct, setSelectedProduct] = useState(null);
-
-  const openModal = (product) => {
-    setSelectedProduct(product);
-  };
-
-  const closeModal = () => {
-    setSelectedProduct(null);
-  };
-
-  const isMobile = window.innerWidth <= 767; // ウィンドウ幅が767px以下の場合にtrueとなる変数を定義
-
+  const columnCount = calculateColumnCount();
 
   return (
-    <div className={`product-list ${isMobile ? 'mobile' : ''}`}>
+    <div className="product-list" style={{ gridTemplateColumns: `repeat(${columnCount}, 1fr)` }}>
       {products.map((product) => (
-        <div
-          key={product.id}
-          className="product-item"
-          onClick={() => openModal(product)}
-        >
-          <div className={`product-item-border ${isMobile ? 'mobile' : ''}`}>
+        <div key={product.id} className="product-item">
+          <div className="product-item-border">
             <h3>{product.name}</h3>
             <p>{product.description}</p>
             <p>${product.price}</p>
           </div>
         </div>
       ))}
-
-      {/* モーダル */}
-      {selectedProduct && (
-        <div className="modal">
-          <div className="modal-content">
-            <h3>{selectedProduct.name}</h3>
-            <p>{selectedProduct.description}</p>
-            <p>${selectedProduct.price}</p>
-            <button onClick={closeModal}>Close</button>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
 
 export default ProductList;
-    
